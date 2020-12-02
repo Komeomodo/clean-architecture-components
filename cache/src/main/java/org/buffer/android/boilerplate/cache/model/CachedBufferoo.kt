ca143aa180bf332e2d94e1c7c3b14b9b889debe0
kome <omodo.oghenekome@decagon.dev>
2020-12-02 23:22:00 +0100

    Initial Commit

---
+++ cache/src/main/java/org/buffer/android/boilerplate/cache/model/CachedBufferoo.kt
@@ -0,0 +1,18 @@
+package org.buffer.android.boilerplate.cache.model
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import org.buffer.android.boilerplate.cache.db.constants.BufferooConstants
+
+/**
+ * Model used solely for the caching of a bufferroo
+ */
+@Entity(tableName = BufferooConstants.TABLE_NAME)
+data class CachedBufferoo(
+
+        @PrimaryKey
+        var id: Long,
+        val login: String,
+        val avatar_url: String
+
+)
---
+++ presentation/src/main/java/org/buffer/android/boilerplate/presentation/model/BufferooView.kt
@@ -0,0 +1,6 @@
+package org.buffer.android.boilerplate.presentation.model
+
+/**
+ * Representation for a [BufferooView] instance for this layers Model representation
+ */
+class BufferooView(val login: String, val avatar_url: String)
